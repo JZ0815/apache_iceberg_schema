6480604c1fd7c12d0adb66e7f6f4496c3441fd08
Just Coding <jzhu@github.com>
2023-09-13 15:44:57 -0400

    test

--- src/main/java/IcebergSchemaFromJavaClass.java
+++ src/main/java/IcebergSchemaFromJavaClass.java
@@ -2,50 +2,57 @@ import org.apache.iceberg.Schema;
 import org.apache.iceberg.types.Type;
 import org.apache.iceberg.types.Types;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class IcebergSchemaFromJavaClass {
-    public static Set<Class<?>> proccessedClasses = new HashSet<>();
     private static AtomicInteger fieldId = new AtomicInteger(1);
-    public static Schema generateSchema(Class<?> clazz) throws Exception {
+
+    public static Schema generateSchema(Class<?> clazz) {
         List<Types.NestedField> fields = getNestedFieldList(clazz);
         return new Schema(fields);
     }
 
-    private static Type convertJavaTypeToIcebergType(Class<?> type) throws Exception {
-        if (proccessedClasses.contains(type)) {
-            return null;
-        }
-        proccessedClasses.add(type);
-        if (type == int.class || type == Integer.class) {
+    private static Type convertJavaTypeToIcebergType(Class<?> fieldType) {
+        if (fieldType == int.class || fieldType == Integer.class) {
             return Types.IntegerType.get();
-        } else if (type == String.class) {
+        } else if (fieldType == String.class) {
             return Types.StringType.get();
-        } else if (List.class.isAssignableFrom(type)) {
-            return Types.ListType.ofOptional(fieldId.getAndIncrement(), Types.StructType.of(getNestedFieldList(type)));
-        } else if (Map.class.isAssignableFrom(type)) {
-            return Types.MapType.ofOptional(fieldId.getAndIncrement(), fieldId.getAndIncrement(), Types.StringType.get(), Types.StructType.of());
         } else {
-            List<Types.NestedField> nestedFields = getNestedFieldList(type);
-            return Types.StructType.of(nestedFields);
+            return Types.StructType.of(getNestedFieldList(fieldType));
         }
     }
 
-    private static List<Types.NestedField> getNestedFieldList(Class<?> type) throws Exception {
+    private static Type convertJavaGenericTypeToIcebergType(Class<?> fieldType, java.lang.reflect.Type genericType) {
+        if (List.class.isAssignableFrom(fieldType)) {
+            Type elementType = convertJavaTypeToIcebergType((Class<?>) ((ParameterizedType) genericType).getActualTypeArguments()[0]);
+            return Types.ListType.ofOptional(fieldId.getAndIncrement(), elementType);
+        } else if (Map.class.isAssignableFrom(fieldType)) {
+            Type keyType = Types.StringType.get(); // Map key type is assumed to be a string
+            Type valueType = convertJavaTypeToIcebergType((Class<?>) ((ParameterizedType) genericType).getActualTypeArguments()[1]);
+            return Types.MapType.ofOptional(fieldId.getAndIncrement(), fieldId.getAndIncrement(), keyType, valueType);
+        } else {
+            return convertJavaTypeToIcebergType(fieldType);
+        }
+    }
+
+    private static List<Types.NestedField> getNestedFieldList(Class<?> clazz) {
         List<Types.NestedField> nestedFields = new ArrayList<>();
-        Method[] declaredMethods = type.getMethods();
-        for (Method method : declaredMethods) {
-            if (method.getName().startsWith("get") && method.getParameterCount() == 0) {
-                String fieldName = method.getName().substring(1);
-                fieldName = Character.toLowerCase(fieldName.charAt(0)) + fieldName.substring(1);
-                Type icebergType = convertJavaTypeToIcebergType(method.getReturnType());
-                if(icebergType != null) {
-                    nestedFields.add(Types.NestedField.optional(fieldId.getAndIncrement(), fieldName, icebergType));
-                }
+        Field[] fields = clazz.getDeclaredFields();
+        for (Field field : fields) {
+            String fieldName = field.getName();
+            Type icebergType;
+            if (List.class.isAssignableFrom(field.getType()) || Map.class.isAssignableFrom(field.getType())) {
+                icebergType = convertJavaGenericTypeToIcebergType(field.getType(), field.getGenericType());
+            } else {
+                icebergType = convertJavaTypeToIcebergType(field.getType());
             }
+            nestedFields.add(Types.NestedField.optional(fieldId.getAndIncrement(), fieldName, icebergType));
         }
         return nestedFields;
     }
+
 }
